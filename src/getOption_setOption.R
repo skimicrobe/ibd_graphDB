11cf6cb81aea323fe11fc917b224f58222427901
skimicrobe <54378831+skimicrobe@users.noreply.github.com>
2022-05-27 13:18:53 -0500

    Update getOption_setOption.R

--- src/getOption_setOption.R
+++ src/getOption_setOption.R
@@ -1,14 +1,14 @@
-#-------------------------------------------------------------#
+###############################################################
 # Goal: Getoption seption                                     #
 # - Make the globalenvironment 'stringsAsFactors' == FALSE    #
 #                                                             #
-# Name: Suyeon Kim                                            #
+# Author: Suyeon Kim                                          #
 # Creation Date: Oct-08-2021                                  #
 # Last updated: Oct-08-2021                                   #
-#-------------------------------------------------------------#
+###############################################################
 
-## If Global environment for "stringsAsFactors: is equal to FALSE, it is good. 
-## Otherwise, "stringsAsFactors" is equal to TRUE, change the environment to FALSE
+# If Global environment for "stringsAsFactors: is equal to FALSE, it is good. 
+# Otherwise, "stringsAsFactors" is equal to TRUE, change the environment to FALSE
 if(getOption("stringsAsFactors")) options(stringsAsFactors=F)
 
 getOption("stringsAsFactors")

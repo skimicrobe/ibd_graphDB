f26f77c18184c4d7e2d1694c6b8fd77c4f160455
skimicrobe <54378831+skimicrobe@users.noreply.github.com>
2022-05-27 14:05:38 -0500

    Update taxonAbundance_generator_Aim1.R

--- src/taxonAbundance_generator_Aim1.R
+++ src/taxonAbundance_generator_Aim1.R
@@ -1,11 +1,10 @@
-#-----------------------------------------------------------------------------------------#
-# Goal: This script is for generating abundance matrix according to different             #
-#       level of taxonomy                                                                 #
-#                                                                                         #
-# Date: Nov-01-2021                                                                       #
-# Last updated: Apr-08-2022                                                               #
-# Name: Suyeon Kim                                                                        #
-#-----------------------------------------------------------------------------------------#
+################################################################################
+# Goal: This script is for generating abundance matrix according to different  #
+#       level of taxonomy.                                                     #
+# Author: Suyeon Kim                                                           #
+# Date: Nov-01-2021                                                            #
+# Last updated: Apr-08-2022                                                    #
+################################################################################
 aggregate_dat<- function(mvx_ab, split_taxon, columnName) {
   # Get the index of taxon of interest. 
   columnIndex<- which(colnames(split_taxon) == columnName)
@@ -43,29 +42,3 @@ save_output<- function(abundance_dat, taxonName, data_type){
   fileN= paste(taxonName, data_type, "abundance", "csv", sep=".")
   write.csv(abundance_dat, fileN, row.names=F, quote=F)
 }
-
-
-## Test Aggregate function. 
-#aggregate_ab<- function(mvx_ab, split_taxon, columnName) {
-
-## Create a lineage 
-## ASK Ishwor [Character vs Numeric] 
-#  columnIndex <- which(colnames(split_taxon)== columnName)
-
-#  columns_to_select <- colnames(split_taxon)[1:(columnIndex-1)]
-
-#  lineage<- subset(split_taxon, select=columns_to_select)
-
-#  alineage <- apply(lineage, 1, paste, collapse='|')
-
-## Combine lineage with original abundance data 
-#  new_table<- cbind("ancestor"=alineage, "leaf"=split_taxon[,columnIndex],mvx_ab)
-#  colnames(new_table)    
-
-## Generate abundance matrix 
-#  aggre_dat<- aggregate( new_table[,!(names(new_table) %in% colnames(lineage))], by=list(new_table$Kingdom, new_table$Phylum, new_table$Class, new_table$Order, new_table$Family, new_table$Genera), FUN=sum, na.rm=T)
-#filt_table<- aggregate(.~Kingdom+Phylum+Class+Order+Family+Genera, data= new_table, sum, na.rm=T)  
-#  agg_table <- aggregate(new_table[,c(-1,-2)], by=list(new_table$ancestor, new_table$leaf),FUN=sum, na.rm=T)
-
-#  return(filt_table)
-#}
